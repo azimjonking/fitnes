190cf480108e1d3c26dfaae10b94ad97a19aa444
azimjonking <azimjonjalilov1301@mail.ru>
2024-03-22 22:34:29 +0200

    contact section is done

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -3,7 +3,20 @@ import "./contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='contact container'>
+      <h1 className="title">Напишите нам</h1>
+      <p className="subtitle">Мы всегда готовы ответить на ваши вопросы, предоставить дополнительную информацию и поддержать вас.</p>
+
+      <form action="#" className="contact-form">
+        <input type="text" placeholder='Имя' id='name' name='name' />
+        <input type="email" placeholder='E-mail' id='email' name='email' />
+        <input type="tel" placeholder='Номер телефона' id='phone' name='phone' />
+        <textarea name="message" id="message" placeholder='Сообщение' ></textarea>
+
+        <button className="btn btn-primary">Отправить</button>
+      </form>
+
+    </section>
   )
 }
 
