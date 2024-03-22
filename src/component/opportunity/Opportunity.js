b8d52ae7d64398c07ea8e0c61f9256e5de608b35
azimjonking <azimjonjalilov1301@mail.ru>
2024-03-22 14:53:51 +0200

    opportunity section is done

--- src/component/opportunity/Opportunity.js
+++ src/component/opportunity/Opportunity.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import "./opportunity.css";
+import optionsData from "./optionsData";
+
 
 const Opportunity = () => {
   return (
-    <div>Opportunity</div>
+    <section className='opportunity container'>
+      <h1 className="title">Уникальные возможности для перевоплощения вашего тела</h1>
+      <p className="subtitle">Раскройте свой потенциал с нашим мобильным приложением, где бы вы не находились.</p>
+
+      <div className="opportunity-container">
+        {optionsData.map((option, index) => {
+          return (
+            <div className="option" key={index}>
+              <img src={option.src} alt={option.title} />
+              <h2 className="option-title">{option.title}</h2>
+              <p className="option-text">{option.text}</p>
+            </div>
+          )
+        })}
+      </div>
+
+      <button className="btn btn-primary">Скачайте сейчас</button>
+
+    </section>
   )
 }
 
