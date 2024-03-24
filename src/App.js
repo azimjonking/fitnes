e641b47842e3623031598800b201fdc04edfeddd
azimjonking <azimjonjalilov1301@mail.ru>
2024-03-24 22:56:58 +0200

    footer media is done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect }  from "react";
 import { Routes, Route, useLocation } from 'react-router-dom';
 import "./App.css";
 import Header from "./component/header/Header";
-// import Footer from "./component/footer/Footer";
+import Footer from "./component/footer/Footer";
 // import Home from "./pages/home/Home";
 // import Privacy from "./pages/privacy/Privacy";
 // import Cookie from "./pages/cookie/Cookie";
@@ -24,7 +24,7 @@ function App() {
         {/* <Route path="/privacy" exact element={<Privacy />} /> */}
         {/* <Route path="/cookie" exact element={<Cookie />} /> */}
       {/* </Routes> */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
